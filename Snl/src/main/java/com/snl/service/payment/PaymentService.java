549caf4f2005ee542b3a988737dfeb7f50e548b4
kyseleee <kyseleee@gmail.com>
2015-06-17 19:44:31 +0900

    .

--- Snl/src/main/java/com/snl/service/payment/PaymentService.java
+++ Snl/src/main/java/com/snl/service/payment/PaymentService.java
@@ -12,6 +12,8 @@ public interface PaymentService {
 	
 	public void updatePayment(Payment payment) throws Exception;
 	
+	public void updatePaymentSameReceipt(Payment payment) throws Exception;
+	
 	public void deletePayment(int payNo) throws Exception;
 	
 	public Payment getMonthlyPayment(int groupNo, String month) throws Exception;
@@ -23,4 +25,6 @@ public interface PaymentService {
 	public Map<String,Object> getPaymentListByMonthDuration(int groupNo, String startDate, String endDate) throws Exception;
 	
 	public Map<String,Object> getPaymentListByDay(int groupNo) throws Exception;
+	
+	public Map<String,Object> getPaymentListByDayDuration(int groupNo, String startDate, String endDate) throws Exception;
 }
