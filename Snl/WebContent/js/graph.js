0d7058bf552c2dce22a557117326de7977210095
kyseleee <kyseleee@gmail.com>
2015-06-17 19:47:25 +0900

    .

--- Snl/WebContent/js/graph.js
+++ Snl/WebContent/js/graph.js
@@ -1,7 +1,6 @@
 var chart;
 var groupNoHid=document.getElementById("groupNoHidden").value;
 		$(document).ready(function () {
-			alert("graph");
 			chart = new Highcharts.Chart({
 					chart: {
 						renderTo: 'container',
@@ -47,43 +46,62 @@ var groupNoHid=document.getElementById("groupNoHidden").value;
 	            verticalAlign: 'middle',
 	            borderWidth: 0
 	        },
+	        noData: {
+	        	style: {"fontSize": "22px", "fontWeight": "bold", "color": "#60606a"}
+	        },
 	        series: []
 	    });
+			
+//		Highcharts.setOptions({
+//	        lang: {
+//	            noData: "데이터가 없습니다"
+//	        }
+//	    });
 	});
 	
-	function requestData(){
+	function requestData() {
 		$.ajax({
 				url:'./listPaymentByMonth.do',
 				type: "GET",
 				data: {groupNo : groupNoHid},
 				success: function(data){
-						data=JSON.parse(data); 
-						chart.addSeries({
-								name: "group1",
-								data: data.Group
-						});
+						alert(data);
+						data=JSON.parse(data);
+						alert(data.Group);
+						if(data.Group=="nodata") {
+							alert("111");
+							chart.hideNoData();
+							chart.showNoData("데이터가 없습니다");
+						}
+						else {
+							alert("222");
+							chart.addSeries({
+									name: data.name,
+									data: data.Group
+							});
+						}
 				},
 				cache: false
 			});
 	}
 	
-	function updateChart(){
-		var chart = $('#container').highcharts();
-		alert($('#startDate').val()+"++"+$('#endDate').val());
-		$.ajax({
-			url:'./listPaymentByMonth.do',
-			type: "POST",
-			data: {groupNo : groupNoHid, startDate : $('#startDate').val(), endDate : $('#endDate').val()},
-			success: function(data){
-				alert(data);
-				var chart = $('#container').highcharts();
-					data=JSON.parse(data);
-					alert(data);
-					chart.series[0].setdata("[0,0,0,1000,63500,50000,7000,0,0,30000]");
-			},
-			cache: false
-		});
-	}
+//	function updateChart() {
+//		var chart = $('#container').highcharts();
+//		alert($('#startDate').val()+"++"+$('#endDate').val());
+//		$.ajax({
+//			url:'./listPaymentByMonth.do',
+//			type: "POST",
+//			data: {groupNo : groupNoHid, startDate : $('#startDate').val(), endDate : $('#endDate').val()},
+//			success: function(data){
+//				alert(data);
+//				var chart = $('#container').highcharts();
+//					data=JSON.parse(data);
+//					alert(data);
+//					chart.series[0].setdata("[0,0,0,1000,63500,50000,7000,0,0,30000]");
+//			},
+//			cache: false
+//		});
+//	}
 	
 	$(function () {
 		$.each(['line', 'column', 'area'], function (i, type) {
@@ -96,18 +114,74 @@ var groupNoHid=document.getElementById("groupNoHidden").value;
 		
 		$('#duration').click(function () {
 				alert($('#startDate').val()+"++"+$('#endDate').val());
-				$.ajax({
-					url:'./listPaymentByMonth.do',
-					type: "POST",
-					data: {groupNo : groupNoHid, startDate : $('#startDate').val(), endDate : $('#endDate').val()},
-					success: function(data){
-						alert(data);
-						var chart = $('#container').highcharts();
-							data=JSON.parse(data);
-							chart.series[0].setData(data.Group);
-					},
-					cache: false
-				});
+				startYear=parseInt($('#startDate').val().substring(0,4));
+				startMonth=parseInt($('#startDate').val().substring(5));
+				endYear=parseInt($('#endDate').val().substring(0,4));
+				endMonth=parseInt($('#endDate').val().substring(5));
+				alert(endYear-startYear);
+				
+				
+				if(startYear<=endYear) {
+					if(startYear<endYear) {
+						if(((endMonth+(12*(endYear-startYear)))-startMonth+1) >12) {
+							alert((endMonth+(12*(endYear-startYear)))-startMonth+1);
+							$('#errMsg').html("<font color='red'>검색기간은 12개월이 넘으면 안됩니다!</font>");
+						}
+						else {
+							$('#errMsg').html("");
+							$.ajax({
+								url:'./listPaymentByMonth.do',
+								type: "POST",
+								data: {groupNo : groupNoHid, startDate : $('#startDate').val(), endDate : $('#endDate').val()},
+								success: function(data){
+									alert(data);
+									var chart = $('#container').highcharts();
+										data=JSON.parse(data);
+										if(data.Group=="nodata") {
+											chart.hideNoData();
+											chart.showNoData("데이터가 없습니다");
+										}
+										else {
+											chart.series[0].setData(data.Group);
+										}
+								},
+								cache: false
+							});
+							
+						}
+					}
+					else {
+						if(startMonth>endMonth) {
+							$('#errMsg').html("<font color='red'>시작일이 종료일보다 빠릅니다!</font>");
+						}
+						else {
+							$('#errMsg').html("");
+							$.ajax({
+								url:'./listPaymentByMonth.do',
+								type: "POST",
+								data: {groupNo : groupNoHid, startDate : $('#startDate').val(), endDate : $('#endDate').val()},
+								success: function(data){
+									alert(data);
+									var chart = $('#container').highcharts();
+										data=JSON.parse(data);
+										if(data.Group=="nodata") {
+											chart.hideNoData();
+											chart.showNoData("데이터가 없습니다");
+										}
+										else {
+											chart.series[0].setData(data.Group);
+										}
+								},
+								cache: false
+							});
+						}
+					}
+				}
+				else {
+					$('#errMsg').html("<font color='red'>시작일이 종료일보다 빠릅니다!</font>");
+				}
+				
+				
 	    });
 		
 	   
---
+++ Snl/WebContent/js/listPayment2.js
@@ -0,0 +1,84 @@
+var groupNoHid=document.getElementById("groupNoHidden").value;
+function popup(url) {
+	window.open(url,'','width=460, height=460, scrollbars=no, status=no;');
+}
+$(document).ready(function(){
+    $('.filterable .btn-filter').click(function(){
+        var $panel = $(this).parents('.filterable'),
+        $filters = $panel.find('.filters input'),
+        $tbody = $panel.find('.table tbody');
+        if ($filters.prop('disabled') == true) {
+            $filters.prop('disabled', false);
+            $filters.first().focus();
+        } else {
+            $filters.val('').prop('disabled', true);
+            $tbody.find('.no-result').remove();
+            $tbody.find('tr').show();
+        }
+    });
+
+    $('.filterable .filters input').keyup(function(e){
+        /* Ignore tab key */
+        var code = e.keyCode || e.which;
+        if (code == '9') return;
+        /* Useful DOM data and selectors */
+        var $input = $(this),
+        inputContent = $input.val().toLowerCase(),
+        $panel = $input.parents('.filterable'),
+        column = $panel.find('.filters th').index($input.parents('th')),
+        $table = $panel.find('.table'),
+        $rows = $table.find('tbody tr');
+        /* Dirtiest filter function ever ;) */
+        var $filteredRows = $rows.filter(function(){
+            var value = $(this).find('td').eq(column).text().toLowerCase();
+            return value.indexOf(inputContent) === -1;
+        });
+        /* Clean previous no-result if exist */
+        $table.find('tbody .no-result').remove();
+        /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
+        $rows.show();
+        $filteredRows.hide();
+        /* Prepend no-result row if all rows are filtered */
+        if ($filteredRows.length === $rows.length) {
+            $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No result found</td></tr>'));
+        }
+    });
+    
+    $.ajax({
+		url : './listPaymentByDay.do',
+		data : {groupNo : groupNoHid},
+		dataType : 'json',
+		method : 'POST',
+		success : function(data){
+			alert(decodeURIComponent(data[0].payName));
+			var receiptLink;
+			for(var i=0;i<data.length;i++){
+				var row = $('<tr></tr>');
+				var content='';
+				data[i].method=decodeURIComponent(data[i].method);
+				data[i].receit=decodeURIComponent(data[i].receit);
+				data[i].payName=decodeURIComponent(data[i].payName);
+				
+				if(data[i].method=='1') {
+					data[i].method='신용카드';
+				} else {
+					data[i].method='현    금';
+				}
+				
+				if(data[i].receit=='-') {				
+					receiptLink=data[i].receit;
+				}
+				else {
+					receiptLink='<a href="http://192.168.200.122:8080/Snl/'+data[i].receit+'" onclick="popup(this.href); return false;">'+data[i].receit+'</a>';
+				}
+				
+				content+='<td>'+(i+1)+'</td><td>'+data[i].method+'</td><td>'+data[i].payDate+'</td><td>'+data[i].payName+'</td><td>'+parseInt(data[i].amount).toLocaleString()+'원'+'</td><td>'+receiptLink+'</td>';
+				$(content).appendTo(row);
+				$('.table > tbody:last').append(row); 
+			}
+		}
+		
+	});
+    
+    
+});
